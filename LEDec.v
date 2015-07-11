258c9886ad29d6ff51601b01432a47d9b8419844
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-07-10 19:31:49 -0500

    Moved results to different files.

---
+++ LEDec.v
@@ -0,0 +1,356 @@
+Require Import Coq.ZArith.BinInt.
+Require Import Coq.Lists.List.
+
+Require Import Vars.
+Require Import Lang.
+Require Import PhaseDiff.
+Require Import PairUtil.
+
+Require TransClosure.
+
+Section LE_DEC.
+Variable pm:phasermap.
+
+Let map_phid_in:
+  forall p,
+  { Map_PHID.In p pm } + { ~ Map_PHID.In p pm }.
+Proof.
+  intros.
+  remember (Map_PHID.mem p pm).
+  symmetry in Heqb.
+  destruct b.
+  - apply Map_PHID_Facts.mem_in_iff in Heqb.
+    intuition.
+  - apply Map_PHID_Facts.not_mem_in_iff in Heqb.
+    intuition.
+Qed.
+
+Let map_tid_in:
+  forall t (ph:phaser),
+  { Map_TID.In t ph } + { ~ Map_TID.In t ph }.
+Proof.
+  intros.
+  remember (Map_TID.mem t ph).
+  symmetry in Heqb.
+  destruct b.
+  - apply Map_TID_Facts.mem_in_iff in Heqb.
+    intuition.
+  - apply Map_TID_Facts.not_mem_in_iff in Heqb.
+    intuition.
+Qed.
+
+Lemma ph_diff_dec:
+  forall ph t t',
+  { exists z, ph_diff ph t t' z } + { ~ exists z, ph_diff ph t t' z }.
+Proof.
+  intros.
+  unfold ph_diff.
+  destruct (map_tid_in t ph).
+  - destruct (map_tid_in t' ph).
+    + apply Map_TID_Extra.in_to_mapsto in i.
+      apply Map_TID_Extra.in_to_mapsto in i0.
+      left.
+      destruct i as (v, Hmt).
+      destruct i0 as (v', Hmt').
+      destruct (get_wait_phase v) as (w, Hw).
+      destruct (get_wait_phase v') as (w', Hw').
+      exists (Z.of_nat w - Z.of_nat w')%Z.
+      exists v.
+      intuition.
+      exists v'.
+      intuition.
+      exists w.
+      intuition.
+      exists w'.
+      intuition.
+    + right.
+      intuition.
+      destruct H as (z, (?, (?, (?, (Hmt, _))))).
+      apply Map_TID_Extra.mapsto_to_in in Hmt.
+      contradiction Hmt.
+  - right.
+    intuition.
+    destruct H as (?, (?, (Hmt, _))).
+    apply Map_TID_Extra.mapsto_to_in in Hmt.
+    contradiction Hmt.
+Qed.
+
+Require Import SigUtil.
+
+Section GET_DIFF.
+Variable ph: phaser.
+Variable t1: tid.
+Variable t2: tid.
+
+Definition get_diff_nil := { _:unit | diff ph t1 t2 = None }.
+
+Definition get_diff_pos := { z | ph_diff ph t1 t2 z /\ ~ (z <= 0) % Z }.
+
+Definition get_diff_ok := { z | ph_diff ph t1 t2 z /\ (z <= 0) % Z }.
+
+Inductive get_diff_result : Type :=
+  | GET_DIFF_NIL : get_diff_nil -> get_diff_result
+  | GET_DIFF_POS : get_diff_pos -> get_diff_result
+  | GET_DIFF_OK : get_diff_ok -> get_diff_result.
+
+Lemma get_diff:
+  get_diff_result.
+Proof.
+  remember (diff ph t1 t2).
+  symmetry in Heqo.
+  destruct o.
+  - destruct (ZArith_dec.Z_le_dec z 0).
+    + refine (GET_DIFF_OK (Sig_yes z)).
+      intuition.
+      apply diff_spec_1.
+      assumption.
+    + refine (GET_DIFF_POS (Sig_yes z)).
+      intuition.
+      apply diff_spec_1.
+      assumption.
+  - refine (GET_DIFF_NIL (Sig_yes tt)).
+    assumption.
+Defined.
+End GET_DIFF.
+
+Lemma ph_le_dec:
+  forall ph t t', 
+  { ph_le ph t t' } + { ~ ph_le ph t t' }.
+Proof.
+  intros.
+  unfold ph_le.
+  remember (get_diff ph t t').
+  symmetry in Heqg.
+  destruct g.
+  - destruct g.
+    right.
+    intuition.
+    destruct H as (?, (Hdiff, _)).
+    assert (Hx := e).
+    apply diff_none with (z:=x0) in Hx.
+    contradiction Hx.
+  - destruct g.
+    right.
+    intuition.
+    destruct H as (z', (?, Hle)).
+    destruct a as (?, Hle').
+    assert (z' = x). {
+      apply ph_diff_fun with (ph:=ph) (t:=t) (t':=t'); repeat assumption.
+    }
+    subst.
+    apply Hle' in Hle.
+    assumption.
+  - destruct g.
+    left.
+    exists x.
+    destruct a.
+    intuition.
+Qed.
+
+Section PM_DIFF.
+Variable t: tid.
+Variable t': tid.
+
+Definition is_ph_le (p:phid) (ph:phaser) := if ph_le_dec ph t t' then true else false.
+
+Definition pm_le := Map_PHID_Extra.filter is_ph_le pm.
+
+Lemma pm_le_spec:
+  forall p ph,
+  Map_PHID.MapsTo p ph pm /\ ph_le ph t t' <-> Map_PHID.MapsTo p ph pm_le.
+Proof.
+  unfold pm_le.
+  intros.
+  rewrite Map_PHID_Extra.filter_spec.
+  unfold is_ph_le.
+  - intuition.
+    + destruct (ph_le_dec ph t t').
+      auto.
+      contradiction H1.
+    + destruct (ph_le_dec ph t t').
+      assumption.
+      inversion H1.
+  - intuition.
+Qed.
+
+Lemma wp_le_dec:
+  { wp_le pm t t' } + { ~ wp_le pm t t' }.
+Proof.
+  unfold wp_le.
+  destruct (Map_PHID_Extra.in_choice pm_le).
+  - left.
+    destruct e as (p, H).
+    apply Map_PHID_Extra.in_to_mapsto in H.
+    destruct H as (ph, H).
+    exists p; exists ph.
+    apply pm_le_spec.
+    assumption.
+  - right.
+    intuition.
+    destruct H as (p, (ph, H)).
+    apply pm_le_spec in H.
+    assert (absurd: exists k, Map_PHID.In (elt:=phaser) k pm_le). {
+      exists p.
+      apply Map_PHID_Extra.mapsto_to_in with (e:=ph).
+      assumption.
+    }
+    apply n in absurd.
+    assumption.
+Qed.
+End PM_DIFF.
+
+(* Tasks registered with a phaser *)
+Definition ph_tids (ph:phaser) := Map_TID_Extra.keys ph.
+
+Lemma ph_tids_spec:
+  forall ph t,
+  In t (ph_tids ph) <->
+  Map_TID.In t ph.
+Proof.
+  intros.
+  unfold ph_tids in *.
+  rewrite Map_TID_Extra.keys_spec; 
+  repeat (intros; intuition).
+Qed.
+
+Definition pm_tids :=
+  flat_map ph_tids (Map_PHID_Extra.values pm).
+
+Lemma pm_tids_spec_1:
+  forall t,
+  In t pm_tids ->
+  exists (p : Map_PHID.key) (ph : phaser), Map_PHID.MapsTo p ph pm /\ Map_TID.In t ph.
+Proof.
+  intros.
+  unfold pm_tids in *.
+  rewrite in_flat_map in H.
+  destruct H as (ph, (Hmt, Hin)).
+  rewrite ph_tids_spec in Hin.
+  apply Map_PHID_Extra.values_spec in Hmt.
+  destruct Hmt as (p, Hmt).
+  exists p; exists ph.
+  intuition.
+Qed.
+
+Lemma pm_tids_spec_2:
+  forall p ph t,
+  Map_PHID.MapsTo p ph pm ->
+  Map_TID.In t ph ->
+  In t pm_tids.
+Proof.
+  intros.
+  unfold pm_tids in *.
+  rewrite in_flat_map.
+  exists ph.
+  rewrite ph_tids_spec.
+  intuition.
+  apply Map_PHID_Extra.values_spec_2 with (k:=p).
+  assumption.
+Qed.
+
+Lemma ph_le_in_pm_tids:
+  forall p ph x y,
+  Map_PHID.MapsTo p ph pm ->
+  ph_le ph x y ->
+  In x pm_tids /\ In y pm_tids.
+Proof.
+  intros.
+  apply ph_le_inv_in in H0.
+  destruct H0.
+  split; repeat(
+    apply pm_tids_spec_2 with (p:=p) (ph:=ph);
+    assumption
+  ).
+Qed.
+
+Lemma wp_le_in_pm_tids:
+  forall x y,
+  wp_le pm x y ->
+  In x pm_tids /\ In y pm_tids.
+Proof.
+  intros.
+  unfold wp_le in *.
+  destruct H as (p, (ph, (Hmt, Hle))).
+  apply ph_le_in_pm_tids with (p:=p) (ph:=ph); repeat auto.
+Qed.
+
+Definition product (t:tid) := map (fun t' => (t, t')) pm_tids.
+
+Lemma wp_le_in_product:
+  forall x y,
+  wp_le pm x y ->
+  In (x, y) (product x).
+Proof.
+  intros.
+  unfold product.
+  apply in_map.
+  apply wp_le_in_pm_tids in H.
+  intuition.
+Qed.
+
+Lemma in_product_inv_eq:
+  forall x y z,
+  In (x, y) (product z) ->
+  x = z.
+Proof.
+  intros.
+  unfold product in *.
+  remember (fun t' : Map_TID.key => (z, t')) as f.
+  rewrite in_map_iff in H.
+  destruct H as (w, (Heq, Hin)).
+  subst.
+  inversion Heq.
+  auto.
+Qed.
+
+Definition all_pairs : list (tid*tid)%type :=
+  flat_map (fun t => product t) pm_tids.
+
+Lemma all_pairs_spec_1:
+  forall x y : tid,
+  wp_le pm x y -> In (x, y) all_pairs.
+Proof.
+  intros.
+  unfold all_pairs.
+  rewrite in_flat_map.
+  exists x.
+  split.
+  + apply wp_le_in_pm_tids with (y:=y); assumption.
+  + apply wp_le_in_product; assumption.
+Qed.
+
+Definition wp_le_rel :=
+  filter (fun (p:(tid*tid)%type) => let (t, t') := p in if wp_le_dec t t' then true else false) all_pairs.
+
+Lemma wp_rels_spec:
+  forall x y : tid,
+  wp_le pm x y <-> In (x, y) wp_le_rel.
+Proof.
+  intros.
+  unfold wp_le_rel.
+  rewrite filter_In.
+  split.
+  - intros.
+    destruct (wp_le_dec x y).
+    + intuition.
+      apply all_pairs_spec_1.
+      assumption.
+    + contradiction H.
+  - intros.
+    destruct H.
+    destruct (wp_le_dec x y).
+    + assumption.
+    + inversion H0.
+Qed.
+
+Lemma LE_dec:
+  forall t t',
+  {LE pm t t'} + {~ LE pm t t'}.
+Proof.
+  unfold LE.
+  intros.
+  apply TransClosure.clos_trans_dec with (pairs:=wp_le_rel).
+  apply wp_rels_spec.
+Qed.
+
+End LE_DEC.
---
+++ Progress.v
@@ -0,0 +1,162 @@
+Require Import Coq.Lists.List.
+
+Require Import Vars.
+Require Import Lang.
+Require Import PhaseDiff.
+Require Import LEDec.
+Require Rel.
+
+Section HAS_SMALLEST.
+Variable pm: phasermap.
+Let IsA t := tid_In t pm.
+
+Let wtid_le (t1:tid) (t2:tid) := LE pm t1 t2.
+
+Let wtid_le_inv := LE_inv pm.
+
+Let wtid_le_trans:
+  forall t1 t2 t3,
+  wtid_le t1 t2 ->
+  wtid_le t2 t3 ->
+  wtid_le t1 t3.
+Proof.
+  unfold wtid_le in *.
+  apply LE_trans.
+Qed.
+
+Let wtid_has_smallest :=
+  Rel.has_smallest tid IsA wtid_le (LE_inv pm) (LE_dec pm)
+  (LE_refl pm) wtid_le_trans.
+
+Definition Smallest (t:tid) (ts:list tid)  :=
+  In t ts /\
+  forall t', In t' ts -> (~ LE pm t t' /\ ~ LE pm t' t) \/ LE pm t t'.
+
+Theorem has_smallest:
+  forall ts,
+  ts <> nil ->
+  Forall IsA ts ->
+  exists t,
+  Smallest t ts.
+Proof.
+  intros.
+  destruct (wtid_has_smallest _ H H0) as (x, Hx).
+  unfold Rel.Smallest in *.
+  unfold Rel.Unrelated in *.
+  unfold wtid_le in *.
+  exists x.
+  auto.
+Qed.
+
+Variable get_registered:
+  forall (t:tid),
+  exists ps, Registered pm t ps.
+
+Variable tids: list tid.
+
+Variable tids_def:
+  forall t, In t tids <-> IsA t.
+
+Let smallest_inv:
+  forall t,
+  Smallest t tids ->
+  In t tids.
+Proof.
+  intros.
+  unfold Smallest in *.
+  intuition.
+Qed.
+
+(** XXX: add support for SO *)
+Variable OnlySW :
+  forall (ph:phaser) (t:tid) (v:taskview),
+  Map_TID.MapsTo t v ph ->
+  exists n, v = SW n true \/ v = WO n \/ exists w, (v = SO n w /\ w < n).
+
+Variable Smallest_to_WaitPhase :
+  forall t t' v v' p ph n n',
+  Smallest t tids ->
+  Map_PHID.MapsTo p ph pm ->
+  Map_TID.MapsTo t v ph ->
+  Map_TID.MapsTo t' v' ph ->
+  WaitPhase v n ->
+  WaitPhase v' n' ->
+  n <= n'.
+
+Lemma smallest_to_sync:
+  forall t p ph,
+  Smallest t tids ->
+  Map_PHID.MapsTo p ph pm ->
+  Sync ph t.
+Proof.
+  intros.
+  remember (Map_TID.find t ph).
+  symmetry in Heqo. 
+  destruct o as [v|].
+  * rewrite <- Map_TID_Facts.find_mapsto_iff in Heqo.
+    destruct (wait_cap_or_sigonly v).
+    - destruct (get_wait_phase v) as (n, Hwp).
+      apply sync_wait with (v:=v) (w:=n); repeat intuition.
+      unfold Await.
+      intros t' v' n' Hmt' Hsp.
+      destruct (OnlySW ph _ _ Hmt') as (n'', [Heq|[Heq|(w, (Heq, Hw))]]).
+      + (* v' is SW *)
+        subst.
+        inversion Hsp.
+        subst.
+        (* WP of v' *)
+        assert (Hwait : WaitPhase (SW n'' true) n''). {
+          apply wait_phase_sw.
+        }
+        (* show that: n <= WP(v') *)
+        assert (Hle : n <= n''). {
+          apply Smallest_to_WaitPhase with
+          (t:=t) (t':=t') (v:=v) (v':=(SW n'' true))
+          (p:=p) (ph:=ph); repeat auto.
+        }
+        intuition.
+      + (* v' is WO *)
+        subst. (* absurd *)
+        inversion Hsp.
+      + (* v' is SO *)
+        subst.
+        inversion Hsp.
+        subst.
+        assert (Hwait : WaitPhase (SO n' w) w). {
+          apply wait_phase_so.
+        }
+        (* show that: n <= WP(v') *)
+        assert (Hle : n <= w). {
+          apply Smallest_to_WaitPhase with
+          (t:=t) (t':=t') (v:=v) (v':=(SO n' w))
+          (p:=p) (ph:=ph); repeat auto.
+       }
+       intuition.
+    - apply sync_so with (v:=v); repeat auto.
+  * rewrite <- Map_TID_Facts.not_find_in_iff in Heqo.
+    apply sync_skip.
+    assumption.
+Qed.
+
+Theorem has_unblocked:
+  tids <> nil ->
+  exists t, In t tids /\
+  exists pm', Reduce pm t WAIT_ALL pm'.
+Proof.
+  intros.
+  assert (Hisa : Forall IsA tids). {
+    apply Forall_forall.
+    intros.
+    apply tids_def.
+    assumption.
+  }
+  assert (Hsmall := has_smallest _ H Hisa).
+  destruct Hsmall as (t, Hsmall).
+  exists t.
+  intuition.
+  exists (mapi t wait pm).
+  apply reduce_wait_all.
+  intros.
+  apply smallest_to_sync with (p:=p) ; repeat auto.
+Qed.
+End HAS_SMALLEST.
