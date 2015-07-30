583e1aa772706adac0a8b7017ac5c3e7054c82ef
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-07-30 16:07:02 -0500

    Added dependency to Aniceto.

--- dev.sh
+++ dev.sh
@@ -3,4 +3,5 @@ version=8.4
 coq_shell_url="https://raw.githubusercontent.com/gares/opam-coq-shell/master/src/opam-coq"
 curl -s "${coq_shell_url}" | bash /dev/stdin init $version && # Install Coq dependencies
 eval `opam config env --switch=coq-shell-$version` &&
-coq_makefile -f _CoqProject -o Makefile
+coq_makefile -f _CoqProject -o Makefile &&
+opam install coq:aniceto || (echo "Install https://bitbucket.org/cogumbreiro/aniceto-coq first."; exit 1)
