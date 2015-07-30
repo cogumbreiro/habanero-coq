8fada9962fb81407f3b77be9551f5b05658d2696
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-07-30 16:04:39 -0500

    Updated the project build scripts.

---
+++ dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+version=8.4
+coq_shell_url="https://raw.githubusercontent.com/gares/opam-coq-shell/master/src/opam-coq"
+curl -s "${coq_shell_url}" | bash /dev/stdin init $version && # Install Coq dependencies
+eval `opam config env --switch=coq-shell-$version` &&
+coq_makefile -f _CoqProject -o Makefile
