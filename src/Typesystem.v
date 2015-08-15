929f9e53b56905f858162a2df70b829bf2a86180
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-08-14 20:29:24 -0500

    Initial version of the type system.

---
+++ src/Typesystem.v
@@ -0,0 +1,53 @@
+Require Import HJ.Lang.
+Require Import HJ.Vars.
+Require Import Coq.Lists.List.
+
+Definition tid_In (t:tid) (pm:phasermap) :=
+  exists p ph, Map_PHID.MapsTo p ph pm /\ Map_TID.In t ph.
+
+Local Open Scope reg_scope.
+
+Inductive CanRegister: tid -> phasermap -> phased -> Prop :=
+  can_register_def:
+    forall p (ph:phaser) pm t v ph r,
+    Map_PHID.MapsTo p ph pm ->
+    Map_TID.MapsTo t v ph ->
+    r <= (mode v) ->
+    CanRegister t pm (p, r).
+
+Inductive Check: tid -> op -> phasermap -> Prop :=
+  | check_ph_new:
+    forall t p pm,
+    ~ Map_PHID.In p pm ->
+    Check t (PH_NEW p) pm
+
+  | check_ph_signal:
+    forall t p ph pm,
+    Map_PHID.MapsTo p ph pm ->
+    Map_TID.In t ph ->
+    Check t (PH_SIGNAL p) pm
+  
+  | check_ph_drop:
+    forall t p ph pm,
+    Map_PHID.MapsTo p ph pm ->
+    Map_TID.In t ph ->
+    Check t (PH_DROP p) pm
+
+  | check_signal_all:
+    forall t pm,
+    Check t SIGNAL_ALL pm
+
+  | check_wait_all:
+    forall t pm,
+    (forall p ph,
+      Map_PHID.MapsTo p ph pm ->
+      forall v,
+      Map_TID.MapsTo t v ph ->
+      v.(wait_phase) < v.(signal_phase)) ->
+    Check t WAIT_ALL pm
+
+  | check_async:
+    forall t t' ps pm,
+    ~ tid_In t' pm ->
+    Forall (CanRegister t pm) ps ->
+    Check t (ASYNC ps t') pm.
