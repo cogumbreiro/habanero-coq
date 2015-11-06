492941ad851b539f55df2a4d830b53557b611be7
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-11-06 10:32:56 -0600

    Defining some properties at the phaser level.

--- src/Phasers/Lang.v
+++ src/Phasers/Lang.v
@@ -5,25 +5,6 @@ Require Export HJ.Phasers.Taskview.
 
 Definition phaser := Map_TID.t taskview.
 
-
-Module Phaser.
-
-  Definition make (t:tid) := Map_TID.add t Taskview.make (Map_TID.empty taskview).
-
-  Definition update (t:tid) (f:taskview -> taskview) (ph:phaser) : phaser :=
-  match Map_TID.find t ph with
-    | Some v => Map_TID.add t (f v) ph
-    | None => ph
-  end.
-
-  Definition signal (t:tid) := update t Taskview.signal.
-
-  Definition wait (t:tid) := update t Taskview.wait.
-
-  Definition drop : tid -> phaser -> phaser := @Map_TID.remove taskview.
-
-End Phaser.
-
 Definition Await (ph:phaser) (n:nat) : Prop :=
   forall t v,
   Map_TID.MapsTo t v ph ->
@@ -46,6 +27,152 @@ Inductive Sync : phaser -> tid -> Prop :=
     forall t ph,
     ~ Map_TID.In t ph ->
     Sync ph t.
+
+Module Phaser.
+
+  Definition make (t:tid) := Map_TID.add t Taskview.make (Map_TID.empty taskview).
+
+  Definition update (t:tid) (f:taskview -> taskview) (ph:phaser) : phaser :=
+  match Map_TID.find t ph with
+    | Some v => Map_TID.add t (f v) ph
+    | None => ph
+  end.
+
+  Definition signal (t:tid) : phaser -> phaser := update t Taskview.signal.
+
+  Definition wait (t:tid) : phaser -> phaser := update t Taskview.wait.
+
+  Definition drop : tid -> phaser -> phaser := @Map_TID.remove taskview.
+
+  Definition register (t:tid) (v:taskview) (ph:phaser) : phaser := 
+  match Map_TID.find t ph with
+    | Some _ => ph
+    | None => Map_TID.add t v ph
+  end.
+
+
+
+  Module Semantics.
+    Inductive op := SIGNAL | WAIT | DROP | REGISTER : taskview -> op.
+    Inductive Reduction (ph:phaser) (t:tid) : op -> phaser -> Prop :=
+    | reduction_signal:
+      Map_TID.In t ph ->
+      Reduction ph t SIGNAL (signal t ph)
+    | reduction_wait:
+      Map_TID.In t ph ->
+      Sync ph t ->
+      Reduction ph t WAIT (wait t ph)
+    | reduction_drop:
+      Map_TID.In t ph ->
+      Reduction ph t DROP (drop t ph)
+    | reduction_register:
+      forall v,
+      ~ Map_TID.In t ph ->
+      Reduction ph t (REGISTER v) (register t v ph).
+    
+    Definition as_tv_op (o:op) :=
+    match o with
+    | SIGNAL => Some Taskview.Semantics.SIGNAL
+    | WAIT =>  Some Taskview.Semantics.WAIT
+    | _ => None
+    end.
+    
+    Lemma as_tv_op_inv_signal:
+      forall o,
+      as_tv_op o = Some Taskview.Semantics.SIGNAL ->
+      o = SIGNAL.
+    Proof.
+      intros.
+      destruct o; auto; simpl in *; try (inversion H).
+    Qed.
+
+    Lemma as_tv_op_inv_wait:
+      forall o,
+      as_tv_op o = Some Taskview.Semantics.WAIT ->
+      o = WAIT.
+    Proof.
+      intros.
+      destruct o; auto; simpl in *; try (inversion H).
+    Qed.
+
+    Lemma ph_update_spec:
+      forall t v f ph,
+      Map_TID.MapsTo t v ph ->
+      update t f ph = Map_TID.add t (f v) ph.
+    Proof.
+      intros.
+      unfold update.
+        remember (Map_TID.find _ _).
+        symmetry in Heqo.
+        destruct o as [v'|].
+        {
+          apply Map_TID_Facts.find_mapsto_iff in Heqo.
+          assert (v' = v) by eauto using Map_TID_Facts.MapsTo_fun.
+          subst; clear Heqo.
+          trivial.
+        }
+        apply Map_TID_Facts.not_find_in_iff in Heqo.
+        contradiction Heqo.
+        eauto using  Map_TID_Extra.mapsto_to_in.
+    Qed.
+
+    Lemma ph_signal_spec:
+      forall t v ph ph',
+      Map_TID.MapsTo t v ph ->
+      Reduction ph t SIGNAL ph' ->
+      ph' = Map_TID.add t (Taskview.signal v) ph.
+    Proof.
+      intros.
+      inversion H0.
+      subst.
+      unfold signal.
+      auto using ph_update_spec.
+    Qed.
+
+    Lemma ph_wait_spec:
+      forall t v ph ph',
+      Map_TID.MapsTo t v ph ->
+      Reduction ph t WAIT ph' ->
+      ph' = Map_TID.add t (Taskview.wait v) ph.
+    Proof.
+      intros.
+      inversion H0.
+      subst.
+      unfold wait.
+      auto using ph_update_spec.
+    Qed.
+
+    Lemma ph_tv_in:
+      forall ph t o o' ph',
+      Reduction ph t o ph' ->
+      as_tv_op o = Some o' ->
+      Map_TID.In t ph.
+    Proof.
+      intros.
+      destruct o'.
+      - apply as_tv_op_inv_signal in H0; subst.
+        inversion H; auto.
+      - apply as_tv_op_inv_wait in H0; subst.
+        inversion H; auto.
+    Qed.
+
+    Lemma ph_to_tv_correct:
+      forall ph t o o' ph' v,
+      Reduction ph t o ph' ->
+      as_tv_op o = Some o' ->
+      Map_TID.MapsTo t v ph ->
+      ph' = Map_TID.add t (Semantics.get_func o' v) ph.
+    Proof.
+      intros.
+      destruct o'; simpl.
+      - apply as_tv_op_inv_signal in H0; subst.
+        auto using ph_signal_spec.
+      - apply as_tv_op_inv_wait in H0; subst.
+        auto using ph_wait_spec.
+    Qed.
+
+  End Semantics.
+End Phaser.
 
 Definition phased := (phid * regmode) % type.
 
