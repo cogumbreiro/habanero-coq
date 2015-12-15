acbebd6fb363d6d14a08921ae79964cb7a67bcc0
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-12-15 14:08:47 -0600

    Added an example.

--- src/Phasers/PhaseOrdering.v
+++ src/Phasers/PhaseOrdering.v
@@ -487,6 +487,38 @@ Module Taskview.
     - rewrite H1 in *.
       inversion H5.
   Qed.
+
+  Example tv_ex_1:
+    {| signal_phase := 0; wait_phase := 0; mode := SIGNAL_WAIT |} <
+    {| signal_phase := 1; wait_phase := 1; mode := SIGNAL_WAIT |}.
+  Proof.
+    eauto using tv_lt_def.
+  Qed.
+
+  Lemma tv_ex_2:
+    forall (v:taskview),
+    SignalPre v ->
+    WaitPre (signal v) ->
+    mode v = SIGNAL_WAIT ->
+    v < (wait (signal v)).
+  Proof.
+    intros.
+    inversion H0; clear H0.
+    apply tv_lt_def.
+    - rewrite wait_wait_phase.
+      rewrite signal_preserves_wait_phase.
+      inversion H.
+      + rewrite H3 in *.
+        intuition.
+      + rewrite H0 in *.
+        inversion H1.
+    - rewrite H1.
+      apply signal_cap_sw.
+    - rewrite wait_preserves_mode.
+      rewrite signal_preserves_mode.
+      rewrite H1.
+      apply wait_cap_sw.
+  Qed.
   End Facts.
 End Taskview.
 
@@ -1138,151 +1170,37 @@ Module Phaser.
     apply ph_s_reduces_preserves_ge_left with (y); auto.
   Qed.
 
-  Section ReducesPreservesAwait.
-  Variable ph: phaser.
-  Variable n: nat.
-  Variable wf: Welformed ph.
-  Variable W: Await ph n.
+  (* 
+  Suppose~$\Ph \Rwait \Phy \Rsignal \Phz$ and that $\Mode[\Ph(\Tid)] = \SW$.
+  %
+  We have that~$\Ph \HB \Phz$.
+  *)
+  Section EX2.
+  Variable x y z: phaser.
+  Variable t: tid.
+  Variable R1: Reduces x t SIGNAL y.
+  Variable R2: Reduces y t WAIT z.
+  Variable v: taskview.
+  Variable mt1: Map_TID.MapsTo t v x.
+  Variable is_sw: mode v = SIGNAL_WAIT.
+  Variable wf: Taskview.Welformed v.
 
-  Lemma ph_signal_preserves_await:
-    forall t,
-    SignalPre t ph ->
-    Await (signal t ph) n.
+  Example ex2:
+    HappensBefore x z.
   Proof.
     intros.
-    apply await_def.
-    intros ? ? mt1 Hs.
-    apply signal_mapsto_inv in mt1.
-    destruct mt1.
-    - destruct a as (E, (v', (R, mt2))).
-      symmetry in E.
-      subst.
-      rewrite signal_preserves_mode in Hs.
-      assert (signal_phase v' >= n) by (inversion W; eauto).
-      assert (signal_phase v' <= signal_phase (Taskview.signal v')). {
-        assert (Taskview.Welformed v') by (inversion wf; eauto).
-        eauto using signal_phase_le_signal.
-      }
-      intuition.
-    - destruct a.
-      inversion W; eauto.
+    inversion R1; simpl in *; clear R1.
+    subst; inversion H; clear H.
+    assert (v0 = v) by eauto using Map_TID_Facts.MapsTo_fun; subst; clear H0.
+    assert (Map_TID.MapsTo t (Taskview.signal v) (signal t x)) by eauto using signal_mapsto_spec.
+    inversion R2; simpl in *; clear R2.
+    subst.
+    assert (Map_TID.MapsTo t (Taskview.wait (Taskview.signal v)) (wait t (signal t x))) by eauto using wait_mapsto_spec.
+    apply ph_happens_before_def with (t1:=t) (t2:=t) (v1:=v) (v2:=(Taskview.wait (Taskview.signal v))); auto.
+    inversion H0.
+    assert (v0 = (Taskview.signal v)) by eauto using Map_TID_Facts.MapsTo_fun; subst; clear H0.
+    apply tv_ex_2; auto.
   Qed.
-
-  Lemma ph_wait_preserves_await:
-    forall t,
-    WaitPre t ph ->
-    Await (wait t ph) n.
-  Proof.
-    intros.
-    apply await_def.
-    intros ? ? mt1 Hs.
-    apply wait_mapsto_inv in mt1.
-    destruct mt1.
-    - destruct a as (E, (v', (R, mt2))).
-      symmetry in E.
-      subst.
-      rewrite wait_preserves_mode in Hs.
-      assert (signal_phase v' >= n) by (inversion W; eauto).
-      rewrite wait_preserves_signal_phase.
-      assumption.
-    - destruct a.
-      inversion W; eauto.
-  Qed.
-
-  Lemma ph_drop_preserves_await:
-    forall t,
-    DropPre t ph ->
-    Await (drop t ph) n.
-  Proof.
-    intros.
-    apply await_def.
-    intros ? ? mt1 Hs.
-    apply drop_mapsto_inv in mt1.
-    destruct mt1.
-    inversion W; eauto.
-  Qed.
-
-  Lemma ph_register_preserves_await:
-    forall r t,
-    RegisterPre r t ph ->
-    Await (register r t ph) n.
-  Proof.
-    intros.
-    apply await_def.
-    intros ? ? mt1 Hs.
-    apply ph_register_inv_mapsto in mt1.
-    destruct mt1.
-    - inversion W; eauto.
-    - destruct H0 as (R1, (v', (mt2, R2))).
-      subst.
-      rewrite set_mode_preserves_signal_phase.
-      inversion W.
-      apply H0 with (t:=t); auto.
-      rewrite mode_set_mode_rw in *.
-      inversion H.
-      assert (v' = v) by eauto using Map_TID_Facts.MapsTo_fun; subst.
-      inversion H3.
-      * rewrite H5 in *.
-        rewrite H6 in *.
-        assumption.
-      * rewrite H5 in *.
-        rewrite H6 in *.
-        assumption.
-      * subst.
-        auto using signal_cap_sw.
-  Qed.
-    
-
-  Lemma ph_reduces_preserves_await:
-    forall ph' t o,
-    Reduces ph t o ph' ->
-    Await ph' n.
-  Proof.
-    intros.
-    destruct o; inversion H; simpl in *; subst.
-    - auto using ph_signal_preserves_await.
-    - auto using ph_wait_preserves_await.
-    - auto using ph_drop_preserves_await.
-    - auto using ph_register_preserves_await.
-  Qed.
-  End ReducesPreservesAwait.
-
-  Section PhReducesAwaitEnabled.
-    Variable ph1 ph2 ph1': phaser.
-    Variable wf1: Welformed ph1.
-    Variable t t': tid.
-    Variable o: op.
-    Variable Hneq: t' <> t.
-    Variable R1: Reduces ph1 t WAIT ph1'.
-    Variable R2: Reduces ph1 t' o ph2.
-
-  Axiom ph_reduces_disjoint:
-    forall ph t o ph' v,
-    t' <> t ->
-    Map_TID.MapsTo t v ph ->
-    Reduces ph t' o ph' ->
-    Map_TID.MapsTo t v ph'.
-
-  Lemma ph_reduces_preserves_wait:
-    Reduces ph2 t WAIT (wait t ph2).
-  Proof.
-    intros.
-    inversion R1;
-    simpl in *; clear R1; subst.
-    inversion H; clear H; subst.
-    assert (Map_TID.MapsTo t v ph2)
-    by eauto using ph_reduces_disjoint.
-    apply ph_reduces.
-    simpl in *.
-    apply wait_pre with (v:=v); auto.
-    inversion H2; subst.
-    - assert (v0 = v) by eauto using Map_TID_Facts.MapsTo_fun; subst.
-      eauto using sync_so.
-    - assert (v0 = v) by eauto using Map_TID_Facts.MapsTo_fun; subst.
-      eauto using sync_wait, ph_reduces_preserves_await.
-  Qed.
-  End PhReducesAwaitEnabled.
-
 End Phaser.
 
 
