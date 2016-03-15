e3408fc4b2fd9e0eca4a076e938390869f047c6f
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2016-03-14 20:22:45 -0500

    Made IEF decidable.

--- src/Finish/IEF.v
+++ src/Finish/IEF.v
@@ -30,6 +30,60 @@ Section IEF.
     unique_child_def:
       (forall x t y, x <= f -> IEF t x -> y < x -> ~ In t y) ->
       UniqueIEF f.
+  Require Import HJ.Finish.Typesystem.
+
+  Lemma child_registered_not_ief:
+    forall x y t,
+    IsMap x ->
+    Child (t <| y) x ->
+    Registered t y ->
+    ~ IEF t x.
+  Proof.
+    intros.
+    unfold not; intros.
+    inversion H1; clear H1.
+    inversion H2.
+    - assert (n: Ready = Blocked y) by eauto using child_fun.
+      inversion n.
+    - assert (R: Blocked x0 = Blocked y) by eauto using child_fun.
+      inversion R; subst; clear R.
+      contradiction H4.
+      eauto using registered_def.
+  Qed.
+
+  Lemma ief_inv_registered:
+    forall t f,
+    IEF t f ->
+    Registered t f.
+  Proof.
+    intros.
+    inversion H;
+    eauto using registered_def.
+  Qed.
+
+  Lemma ief_dec f (I:IsMap f):
+    forall t,
+    { IEF t f } + { ~ IEF t f }.
+  Proof.
+    intros.
+    destruct (lookup_ex t f).
+    - destruct c as (a,?).
+      destruct a as [|f'].
+      + left; auto using ief_ready.
+      + destruct (lookup_ex t f'). {
+          destruct c0.
+          right.
+          eauto using child_registered_not_ief, registered_def.
+        }
+        destruct u.
+        left.
+        eauto using ief_blocked.
+    - destruct u.
+      right.
+      unfold not; intros.
+      contradiction n.
+      auto using ief_inv_registered.
+  Qed.
 
   Lemma ief_cons:
     forall t t' a l,
@@ -38,6 +92,56 @@ Section IEF.
   Proof.
     intros.
     inversion H; eauto using ief_ready, ief_blocked, child_cons.
+  Qed.
+
+  Lemma unique_ief:
+    forall x t y f,
+    UniqueIEF f ->
+    x <= f ->
+    IEF t x ->
+    y < x -> ~ In t y.
+  Proof.
+    intros.
+    inversion H; eauto.
+  Qed.
+
+  Lemma registered_to_ief:
+    forall x t,
+    Registered t x ->
+    IEF t x \/ exists y, y < x /\ IEF t y.
+  Proof.
+    intros.
+    induction x using finish_ind_strong.
+    - apply registered_absurd_nil in H.
+      inversion H.
+    - apply registered_inv_cons in H.
+      destruct H.
+      rewrite <- H in *; clear H.
+      + eauto using ief_ready, child_eq.
+      + apply IHx in H; clear IHx.
+        destruct H.
+        * eauto using ief_cons.
+        * destruct H as (?, (?, ?)).
+          eauto using Rel.lt_cons.
+    - apply registered_inv_cons in H.
+      destruct H.
+      + rewrite <- H in *; clear H.
+        destruct (registered_dec t x). {
+          apply IHx in r; clear IHx.
+          destruct r as [?| (y, (?,?))].
+          - right.
+            exists x.
+            eauto using lt_eq.
+          - right.
+            exists y.
+            eauto using lt_trans, lt_eq.
+        }
+        eauto using ief_blocked, child_eq.
+      + apply IHx0 in H; clear IHx0 IHx.
+        destruct H.
+        * eauto using ief_cons.
+        * destruct H as (?, (?,?)).
+          eauto using Rel.lt_cons.
   Qed.
 
   Lemma ief_in:
@@ -165,16 +269,6 @@ Section Sem.
   Qed.
 
   Require Import HJ.Finish.Typesystem.
-
-  Lemma ief_inv_registered:
-    forall t f,
-    IEF t f ->
-    Registered t f.
-  Proof.
-    intros.
-    inversion H;
-    eauto using registered_def.
-  Qed.
 
   Lemma ief_put_absurd_1:
    forall t x y,
