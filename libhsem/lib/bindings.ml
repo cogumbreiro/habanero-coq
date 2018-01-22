640e000563aa87c8db9f888a4017416b6e7496ca
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-01-22 00:15:34 -0500

    Added some error reporting.

--- libhsem/lib/bindings.ml
+++ libhsem/lib/bindings.ml
@@ -4,6 +4,30 @@ open Finish
 open List
 (* Define a struct of callbacks (C function pointers) *)
 
+let all_ops = [
+  ("INIT", 0);
+  ("BEGIN_FINISH", 1);
+  ("END_FINISH", 2);
+  ("BEGIN_TASK", 3);
+  ("END_TASK", 4)
+]
+
+exception Err of string
+
+let json_to_package j =
+  let open Yojson.Basic.Util in
+  let op = member "op" j |> to_string in
+  try (
+    {
+      Finish.pkg_task = member "task" j |> to_int;
+      Finish.pkg_op = List.find (fun x -> fst x = op) all_ops |> snd;
+      Finish.pkg_id = member "id" j |> to_int;
+      Finish.pkg_time = member "time" j |> to_int;
+      Finish.pkg_arg = member "arg" j |> to_int;
+    }
+  ) with | Not_found -> raise (Err ("Unknown operation " ^ op))
+
+
 let habanero_op = Ctypes.typedef Ctypes.int "enum habanero_op"
 
 type habanero_checks = [ `C ] structure
@@ -11,8 +35,8 @@ type habanero_checks = [ `C ] structure
 let habanero_checks : habanero_checks typ = structure "habanero_checks"
 
 let habanero_checks_new () : habanero_checks ptr =
-    let result = Finish.checks_make in
-    Root.create result |> from_voidp habanero_checks
+  let result = Finish.checks_make in
+  Root.create result |> from_voidp habanero_checks
 
 let habanero_checks_free p =
   if (ptr_compare (to_voidp p) null) == 0
@@ -31,60 +55,60 @@ let a_time = "time" -- int
 let a_arg = "arg" -- int
 let () = seal struct_habanero_action
 
+(** Converts a C habanero_action into a Finish.package *)
+
 let pkg_new a : Finish.package =
-    {
-        Finish.pkg_task = getf a a_task;
-        Finish.pkg_op = getf a a_op;
-        Finish.pkg_id = getf a a_id;
-        Finish.pkg_time = getf a a_time;
-        Finish.pkg_arg = getf a a_arg;
-    }
+  {
+    Finish.pkg_task = getf a a_task;
+    Finish.pkg_op = getf a a_op;
+    Finish.pkg_id = getf a a_id;
+    Finish.pkg_time = getf a a_time;
+    Finish.pkg_arg = getf a a_arg;
+  }
 
-let all_ops = [
-    ("INIT", 0);
-    ("BEGIN_FINISH", 1);
-    ("END_FINISH", 2);
-    ("BEGIN_TASK", 3);
-    ("END_TASK", 4)
-]
-
-
-let json_to_package j =
-    let open Yojson.Basic.Util in
-    let op = member "op" j |> to_string in
-    {
-        Finish.pkg_task = member "task" j |> to_int;
-        Finish.pkg_op = List.find (fun x -> fst x = op) all_ops |> snd;
-        Finish.pkg_id = member "id" j |> to_int;
-        Finish.pkg_time = member "time" j |> to_int;
-        Finish.pkg_arg = member "arg" j |> to_int;
-    }
+let run_err_to_string (r:Finish.run_err) : string =
+  let reduces_err_to_string (e:Finish.reduces_err) =
+    match e with
+    | Finish.TASK_EXIST x -> "Task " ^ string_of_int x ^ " already exists."
+    | Finish.TASK_NOT_EXIST x -> "Task " ^ string_of_int x ^ " missing."
+    | Finish.FINISH_EXIST x -> "Finish " ^ string_of_int x ^ " already exists."
+    | Finish.FINISH_NOT_EXIST x -> "Finish " ^ string_of_int x ^ " missing."
+    | Finish.FINISH_NONEMPTY x -> "Finish " ^ string_of_int x ^ " is not empty."
+    | Finish.FINISH_TOP_NEQ x -> "Finish " ^ string_of_int x ^ " is not the inner enclosing finish."
+    | Finish.FINISH_OPEN_EMPTY -> "Task has no open finish scopes."
+  in
+  match r with
+  | Finish.PKG_ERROR -> "Parsing action."
+  | Finish.REDUCES_ERROR e -> reduces_err_to_string e
 
 let habanero_check (s:habanero_checks ptr) (a:habanero_action) : int =
-    let ptr = to_voidp s in
-    let s = Root.get ptr in
-    match Finish.checks_add (pkg_new a) s with
-    | Inl s' -> Root.set ptr s'; 1
-    | Inr _ -> 0
+  let ptr = to_voidp s in
+  let s = Root.get ptr in
+  match Finish.checks_add (pkg_new a) s with
+  | Inl s' -> Root.set ptr s'; 1
+  | Inr _ -> 0
 
-let habanero_count (s: habanero_checks ptr) : int =
-    to_voidp s |> Root.get |> Finish.count_enqueued
+let habanero_count (s: habanero_checks ptr): int =
+  to_voidp s |> Root.get |> Finish.count_enqueued
 
-exception Err of string
-
-let habanero_parse (filename:string) : habanero_checks ptr =
-    let stream_file c = Stream.from (fun _ ->
-         try Some (input_line c) with End_of_file -> None) in
-    let chk = ref Finish.checks_make in (* initialize hchecks *)
-    let chan = open_in filename in
+let habanero_parse (filename:string) on_error : habanero_checks ptr =
+  let stream_file c = Stream.from (fun _ ->
+     try Some (input_line c) with End_of_file -> None) in
+  let chk = ref Finish.checks_make in (* initialize hchecks *)
+  let chan = open_in filename in
+  try (
     Stream.iter (fun line ->
-        let pkg = Yojson.Basic.from_string line |> json_to_package in
-        match Finish.checks_add pkg !chk with
-        | Inl s' -> chk := s'
-        | Inr _ -> raise (Err "Error parsing")
+      let pkg = Yojson.Basic.from_string line |> json_to_package in
+      match Finish.checks_add pkg !chk with
+      | Inl s' -> chk := s'
+      | Inr e -> raise (Err (run_err_to_string e))
     ) (stream_file chan);
     close_in chan;
     Root.create (!chk) |> from_voidp habanero_checks
+  ) with
+  | Err e ->
+    on_error e;
+    null |> from_voidp habanero_checks
 
 module Stubs(I : Cstubs_inverted.INTERNAL) =
 struct
@@ -97,6 +121,6 @@ struct
   let () = I.internal "habanero_checks_free" (ptr habanero_checks @-> returning void) habanero_checks_free
   let () = I.internal "habanero_checks_add" (ptr habanero_checks @-> habanero_action @-> returning int)
     habanero_check
-  let () = I.internal "habanero_checks_open" (string @-> returning (ptr habanero_checks)) habanero_parse
+  let () = I.internal "habanero_checks_open" (string @-> funptr (string @-> returning void) @-> returning (ptr habanero_checks)) habanero_parse
   let () = I.internal "habanero_checks_count_enqueued" (ptr habanero_checks @-> returning int) habanero_count
 end
--- libhsem/test/test.c
+++ libhsem/test/test.c
@@ -6,56 +6,59 @@
 
 int main(int argc, char **argv)
 {
-  struct habanero_checks* s = habanero_checks_new();
-  
-  // Example where we initalize, create a task, and remove that task
-  
-  assert(habanero_checks_add(s, (habanero_action) {
-    .task = 1,
-    .op = INIT,
-    .id = 1,
-    .time = 0,
-    .arg = 0
-  }));
-  assert(habanero_checks_count_enqueued(s) == 0);
+    struct habanero_checks* s = habanero_checks_new();
+    
+    // Example where we initalize, create a task, and remove that task
+    
+    assert(habanero_checks_add(s, (habanero_action) {
+      .task = 1,
+      .op = INIT,
+      .id = 1,
+      .time = 0,
+      .arg = 0
+    }));
+    assert(habanero_checks_count_enqueued(s) == 0);
 
-  assert(habanero_checks_add(s, (habanero_action) {
-    .task = 1,
-    .op = BEGIN_TASK,
-    .id = 1,
-    .time = 1,
-    .arg = 2
-  }));
-  assert(habanero_checks_count_enqueued(s) == 0);
+    assert(habanero_checks_add(s, (habanero_action) {
+      .task = 1,
+      .op = BEGIN_TASK,
+      .id = 1,
+      .time = 1,
+      .arg = 2
+    }));
+    assert(habanero_checks_count_enqueued(s) == 0);
 
-  assert(habanero_checks_add(s, (habanero_action) {
-    .task = 2,
-    .op = BEGIN_FINISH,
-    .id = 2,
-    .time = 0,
-    .arg = 0
-  }));
-  assert(habanero_checks_count_enqueued(s) == 0);
-  
-  assert(habanero_checks_add(s, (habanero_action) {
-    .task = 2,
-    .op = END_FINISH,
-    .id = 2,
-    .time = 1,
-    .arg = 0
-  }));
-  assert(habanero_checks_count_enqueued(s) == 0);
+    assert(habanero_checks_add(s, (habanero_action) {
+      .task = 2,
+      .op = BEGIN_FINISH,
+      .id = 2,
+      .time = 0,
+      .arg = 0
+    }));
+    assert(habanero_checks_count_enqueued(s) == 0);
+    
+    assert(habanero_checks_add(s, (habanero_action) {
+      .task = 2,
+      .op = END_FINISH,
+      .id = 2,
+      .time = 1,
+      .arg = 0
+    }));
+    assert(habanero_checks_count_enqueued(s) == 0);
 
-  assert(habanero_checks_add(s, (habanero_action) {
-    .task = 1,
-    .op = END_TASK,
-    .id = 1,
-    .time = 2,
-    .arg = 0
-  }));
-  assert(habanero_checks_count_enqueued(s) == 0);
+    assert(habanero_checks_add(s, (habanero_action) {
+      .task = 1,
+      .op = END_TASK,
+      .id = 1,
+      .time = 2,
+      .arg = 0
+    }));
+    assert(habanero_checks_count_enqueued(s) == 0);
 
-  habanero_checks_free(s);
-  puts("All tests passed!");
-  return 0;
+    habanero_checks_free(s);
+    
+    assert(habanero_checks_open("example.json", &puts) == NULL);
+    
+    puts("All tests passed!");
+    return 0;
 }
