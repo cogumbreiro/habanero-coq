165ba267b9a080e4069018786f7e5eb967713d2b
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-02-12 16:55:28 -0500

    Added some documentation to the build script.

--- libhsem/myocamlbuild.ml
+++ libhsem/myocamlbuild.ml
@@ -6,6 +6,8 @@ let () =
       Options.use_ocamlfind := true;
       Options.make_links := false;
     | After_rules ->
+      (* Rules to use our code generator (which is implicitly built)
+         to genreate the ctypes stubs/bindings and the C header. *)
       let generator = "codegen/generate.native" in
       let gen_dir = "generated" in
       rule "generates ctypes stubs"
@@ -16,10 +18,14 @@ let () =
           Cmd (S [A (env generator); A (env gen_dir)]);
         ]
       );
+      (* `apply_bindings.ml` has a dynamic dependency on a generated file `hsem_bindings.ml`. *)
+      (* 1. ensure that we compile `hsem_bindings` before compiling `apply_bindings.ml` *)
       dep ["ocaml"; "compile"; "file:lib/apply_bindings.ml"] [gen_dir ^ "/hsem_bindings.cmx"];
+      (* 2. ensure that the compiler is aware of the code generator directory,
+            so that it can see the generated file. *)
       flag ["ocaml"; "compile"; "file:lib/apply_bindings.ml"] (S [A"-I"; A gen_dir]);
 
-      ocaml_lib "src/base";
+      (* XXX: In the orignal makefile we had these flags set; not sure if actually neede. *)
       flag ["compile"; "c"; "file:hsem_stubs.c"] (S [ A"-cc"; A"gcc -fPIC";]);
       flag ["compile"; "c"; "file:init.c"] (S [ A"-cc"; A"gcc -fPIC";]);
     | _ -> ())
