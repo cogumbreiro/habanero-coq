a3616e2dbbdb293adc9b3a9979d74b9b268ee794
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-02-18 13:22:03 -0500

    Allow accepting multiple filenames.

--- libhsem/bin/main.ml
+++ libhsem/bin/main.ml
@@ -4,27 +4,32 @@ open Yojson.Basic.Util
 open Yojson.Basic
 
 (** Defines the command line option to parse the filename. *)
-let fname =
+let files =
   let doc = "Filename to parse. The file must be JSONL encoded." in
-  Arg.(required & pos 0 (some string) None & info [] ~docv:"FILENAME" ~doc)
-
+  Arg.(non_empty & pos_all file [] & info [] ~docv:"FILE" ~doc)
 
 (** Builds the command line UI *)
 let parse_t =
-  let do_parse fname =
-    try (
-      let s = Hsem.parse fname in
-      let enqueued = Finish.checks_enqueued s in
-      let running = Finish.checks_running s in
-      match enqueued, running with
-      | [], [] -> `Ok true
-      | _, _ ->
-        let js_enq = `List (List.map package_to_json enqueued) |> pretty_to_string in
-        let js_run = `List (List.map (fun x -> `Int x) running) |> pretty_to_string in
-        `Error (false, "There are still operations enqueued or tasks running.\nOperations enqueued: " ^ js_enq ^ "\nTasks running: " ^ js_run)
-    ) with Hsem.Err e -> `Error(false, e)
+  let do_parse fnames =
+    try begin
+      List.iter (fun fname ->
+        try begin
+          let s = Hsem.parse fname in
+          let enqueued = Finish.checks_enqueued s in
+          let running = Finish.checks_running s in
+          match enqueued, running with
+          | [], [] -> ()
+          | _, _ ->
+            let js_enq = `List (List.map package_to_json enqueued) |> pretty_to_string in
+            let js_run = `List (List.map (fun x -> `Int x) running) |> pretty_to_string in
+            raise (Hsem.Err ("There are still operations enqueued or tasks running.\nOperations enqueued: " ^ js_enq ^ "\nTasks running: " ^ js_run))
+        end with Hsem.Err e -> raise (Hsem.Err (fname ^ ": " ^ e)) (* prefix the error with the filename *);
+        ()
+      ) fnames;
+      `Ok true
+    end with Hsem.Err e -> `Error(false, e)
   in
-  Term.(ret (const do_parse $ fname))
+  Term.(ret (const do_parse $ files))
 
 let () = Term.exit @@ Term.eval (parse_t, Term.info "hsem")
 
