4148ed68db88c43f17ac6d7caa5cd2162c3a279c
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-02-18 17:46:24 -0500

    Cleaned up the code.

--- libhsem/bin/main.ml
+++ libhsem/bin/main.ml
@@ -12,20 +12,20 @@ let files =
 let parse_t =
   let do_parse fnames =
     try begin
-      List.iter (fun fname ->
-        try begin
-          let s = Hsem.parse fname in
-          let enqueued = Finish.checks_enqueued s in
-          let running = Finish.checks_running s in
-          match enqueued, running with
-          | [], [] -> ()
-          | _, _ ->
-            let js_enq = `List (List.map package_to_json enqueued) |> pretty_to_string in
-            let js_run = `List (List.map (fun x -> `Int x) running) |> pretty_to_string in
-            raise (Hsem.Err ("There are still operations enqueued or tasks running.\nOperations enqueued: " ^ js_enq ^ "\nTasks running: " ^ js_run))
-        end with Hsem.Err e -> raise (Hsem.Err (fname ^ ": " ^ e)) (* prefix the error with the filename *);
-        ()
-      ) fnames;
+      let f fname = try begin
+        let s = Hsem.parse fname in
+        let enqueued = Finish.checks_enqueued s in
+        let running = Finish.checks_running s in
+        match enqueued, running with
+        | [], [] -> ()
+        | _, _ ->
+          let js_enq = `List (List.map package_to_json enqueued) |> pretty_to_string in
+          let js_run = `List (List.map (fun x -> `Int x) running) |> pretty_to_string in
+          raise (Hsem.Err ("There are still operations enqueued or tasks running.\nOperations enqueued: " ^ js_enq ^ "\nTasks running: " ^ js_run))
+      end with Hsem.Err e ->
+          raise (Hsem.Err (fname ^ ": " ^ e)) (* prefix the error with the filename *)
+      in
+      List.iter f fnames;
       `Ok true
     end with Hsem.Err e -> `Error(false, e)
   in
--- libhsem/src/hsem.ml
+++ libhsem/src/hsem.ml
@@ -15,52 +15,52 @@ exception Err of string
 let string_to_op (o:string) : package_op = List.find (fun x -> fst x = o) all_ops |> snd
 let op_to_string (o:package_op) : string = List.find (fun x -> snd x = o) all_ops |> fst
 
+let err_line_prefix msg lineno =match lineno with
+  | Some l ->
+    let msg = if msg = "" then "" else msg ^ " " in
+    "Error " ^ msg ^ "at line #" ^ string_of_int l ^ ": "
+  | None -> ""
+
 let json_to_package j lineno =
   let open Yojson.Basic.Util in
-  let prefix = match lineno with
-  | Some l -> "Error parsing line #" ^ string_of_int l ^ ": "
-  | None -> ""
-  in
-  try (
+  try begin
     let o : string = member "op" j |> to_string in
-    let replace_null d j = (match j with
-    | `Null -> d
-    | _ -> j)
+    let replace_null d j = match j with
+      | `Null -> d
+      | _ -> j
     in
-    try (
-      {
-        pkg_task = member "task" j |> to_int;
-        pkg_op = string_to_op o;
-        pkg_id = member "id" j |> to_int;
-        pkg_time = member "time" j |> to_int;
-        pkg_args = member "args" j |> replace_null (`List []) |> to_list |> List.map to_int;
-        pkg_lineno = lineno
-      }
-    ) with | Not_found -> raise (Err (prefix ^ "Unknown operation " ^ o))
-  ) with | Type_error (e,_) -> raise (Err (prefix ^ e))
+    try {
+      pkg_task = member "task" j |> to_int;
+      pkg_op = string_to_op o;
+      pkg_id = member "id" j |> to_int;
+      pkg_time = member "time" j |> to_int;
+      pkg_args = member "args" j |> replace_null (`List []) |> to_list |> List.map to_int;
+      pkg_lineno = lineno
+    } with | Not_found -> raise (Err (err_line_prefix "parsing" lineno ^ "Unknown operation " ^ o))
+  end with | Type_error (e,_) -> raise (Err (err_line_prefix "parsing" lineno ^ e))
 
 let package_to_json p =
   let open Yojson.Basic.Util in
+  let lineno = match p.pkg_lineno with
+    | Some x -> `Int x
+    | _ -> `Null
+  in
   `Assoc [
     "pkg_task", `Int (pkg_task p);
     "pkg_op", `String (op_to_string (pkg_op p));
     "pkg_id", `Int (pkg_id p);
     "pkg_time", `Int (pkg_time p);
     "pkg_args", `List (pkg_args p |> List.map (fun x -> `Int x));
-    "pkg_lineno", match p.pkg_lineno with
-      | Some x -> `Int x
-      | _ -> `Null
+    "pkg_lineno", lineno
   ]
 
 
 let run_err_to_string (r:Finish.checks_err) : string =
-  let pkg_parse_err_to_string e =
-  match e with
-  | PKG_PARSE_NOARGS_EXPECTED -> "Expected 0 arguments."
-  | PKG_PARSE_TASK_EXPECTED -> "Expected 1 task identifier."
+  let pkg_parse_err_to_string e = match e with
+    | PKG_PARSE_NOARGS_EXPECTED -> "Expected 0 arguments."
+    | PKG_PARSE_TASK_EXPECTED -> "Expected 1 task identifier."
   in
-  let reduces_err_to_string (e:Finish.reduces_err) =
-    match e with
+  let reduces_err_to_string (e:Finish.reduces_err) = match e with
     | TASK_EXIST x -> "Expecting task " ^ string_of_int x ^ " to be new, but it already exists."
     | TASK_NOT_EXIST x -> "Task " ^ string_of_int x ^ " does not exist."
     | FINISH_EXIST x -> "Expecting finish " ^ string_of_int x ^ " to be new, but it already exists."
@@ -75,11 +75,7 @@ let run_err_to_string (r:Finish.checks_err) : string =
   match r with
   | CHECKS_PARSE_TRACE_ERROR (p, e) -> (
     let args : string = Yojson.Basic.pretty_to_string (`List (pkg_args p |> List.map (fun x -> `Int x))) in
-    (match p.pkg_lineno with
-    | Some n ->
-      "Error parsing line #" ^ string_of_int n ^": "
-    | None -> ""
-    )^ pkg_parse_err_to_string e ^ " Obtained: " ^ args
+    err_line_prefix "parsing" p.pkg_lineno ^ pkg_parse_err_to_string e ^ " Obtained: " ^ args
   )
   | CHECKS_REDUCES_N_ERROR (p, e) -> (
     match p.pkg_lineno with
@@ -94,18 +90,20 @@ let parse (filename:string) =
   let chk = ref Finish.checks_make in (* initialize hchecks *)
   let chan = open_in filename in
   let lineno = ref 0 in
-  Stream.iter (fun line ->
+  let do_iter line =
     lineno := !lineno + 1;
     let line = String.trim line in
-    if (line <> "") && (String.get line 0 <> '#') then (
-      try (
-        let pkg = json_to_package (Yojson.Basic.from_string line) (Some !lineno)  in
+    if (line <> "") && (String.get line 0 <> '#') then begin
+      try begin
+        let j = Yojson.Basic.from_string line in
+        let pkg = json_to_package j (Some !lineno)  in
         match Finish.checks_add pkg !chk with
         | Inl s' -> chk := s'
         | Inr e -> raise (Err (run_err_to_string e))
-      ) with Yojson.Json_error e -> raise (Err("Error parsing line #"^string_of_int (!lineno) ^": " ^ e))
-    ) else () (* nothing to do *)
-  ) (stream_file chan);
+      end with Yojson.Json_error e -> raise (Err(err_line_prefix "parsing" (Some !lineno) ^ e))
+    end
+  in
+  Stream.iter do_iter (stream_file chan);
   close_in chan;
   !chk
 
