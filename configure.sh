2d0aee6fc683a3f353e2e19ad129040ffcc24cec
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-03-21 19:30:13 -0400

    Do not use frozen version.

--- configure.sh
+++ configure.sh
@@ -6,9 +6,6 @@ check_pkg() {
 install_coq() {
   if ! which coqc > /dev/null; then
     echo "Installing Coq over OPAM..."
-    if ! check_pkg coq; then
-      opam repo add coq-released https://coq.inria.fr/opam/released
-    fi
     opam install coq
   fi
 }
